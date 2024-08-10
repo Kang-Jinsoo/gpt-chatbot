68c1c18ecfbc3b07314b7487b2470b8e29896e8e
kangjinsoo <wlstn1435@gmail.com>
2024-08-11 02:55:57 +0900

    publishing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,34 @@
+import "./globals.css";
+
 export default function Home() {
-  return <div>gpt-chatbot</div>;
+  return (
+    <div className="h-full w-2/3 mx-10 border-white">
+      <div className="h-1/6 p-5 text-3xl">chatbot</div>
+      <div className="h-4/6 p-5 overflow-y-auto">
+        <div className="p-3 text-sm text-gray-200">gd</div>
+        <div className="p-3 text-sm text-gray-200">gd</div>
+        <div className="p-3 text-sm text-gray-200">gd</div>
+        <div className="p-3 text-sm text-gray-200">gd</div>
+        <div className="p-3 text-sm text-gray-200">gd</div>
+        <div className="p-3 text-sm text-gray-200">gd</div>
+        <div className="p-3 text-sm text-gray-200">gd</div>
+        <div className="p-3 text-sm text-gray-200">gd</div>
+        <div className="p-3 text-sm text-gray-200">gd</div>
+        <div className="p-3 text-sm text-gray-200">gd</div>
+        <div className="p-3 text-sm text-gray-200">gd</div>
+        <div className="p-3 text-sm text-gray-200">gd</div>
+        <div className="p-3 text-sm text-gray-200">gd</div>
+        <div className="p-3 text-sm text-gray-200">gd</div>
+        <div className="p-3 text-sm text-gray-200">gd</div>
+      </div>
+      <div className="mt-5 h-1/6">
+        <input
+          type="text"
+          placeholder="질문을 해보세요."
+          className="p-2 text-black rounded-xl"
+        />
+        <button className="ml-5 bg-white text-black"> 전송 </button>
+      </div>
+    </div>
+  );
 }
