8d18b608ba1ef4a2f58ea4861768a2f9e9b6c44c
kangjinsoo <wlstn1435@gmail.com>
2024-08-13 01:22:08 +0900

    globals css fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,18 @@
 import "./globals.css";
-import Question from "@/component/question";
-import Answer from "@/component/answer";
+import Question from "@/components/ui/question";
+import Answer from "@/components/ui/answer";
 
 export default function Home() {
   return (
     <div className="h-full border-white">
-      <div className="h-1/6 text-3xl font-mono mx-4 sm:mx-10 md:mx-36 lg:mx-64 xl:mx-96 flex items-center">
+      <div className="h-1/6 text-3xl font-mono mx-custom flex items-center">
         chatBot
       </div>
-      <div className="h-4/6 overflow-y-auto">
-        <div className="mx-4 sm:mx-10 md:mx-36 lg:mx-64 xl:mx-96">
-          <Question />
-          <Answer />
-        </div>
+      <div className="h-4/6 overflow-y-auto mx-custom">
+        <Question />
+        <Answer />
       </div>
-      <div className="h-1/6 mx-4 sm:mx-10 md:mx-36 lg:mx-64 xl:mx-96 flex justify-center items-center">
+      <div className="h-1/6 mx-custom flex justify-center items-center">
         <input
           type="text"
           placeholder="질문을 주세요."
--- src/components/ui/question.tsx
+++ src/components/ui/question.tsx
@@ -1,7 +1,7 @@
 export default function Question() {
   return (
     <div className="flex justify-end mb-5">
-      <div className="px-4 py-3 text-md text-gray-200 rounded-3xl bg-gray-900 content-center">
+      <div className="flex justify-end mb-5 px-4 py-3 text-md rounded-3xl bg-gray-900 content-center">
         질문
       </div>
     </div>
